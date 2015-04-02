be87103eb806a02bfb1c37078daf9d0aca647c5f
webartificer <webartificer@gmail.com>
2015-04-02 03:00:33 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+
+function hideMobileMenu(init)
+{
+    /*If browser resized, check width again */
+    if ($(window).width() > 600) {
+      $('.resmenu').removeClass('mobilemenu');
+      $('.menuswitch').removeClass('active');
+      $("header #hamburger a .on").show();
+      $("header #hamburger a .off").hide();
+    }
+}
+
+
+$(document).on('ready', function() {
+  $( ".menuswitch" ).click(function() {
+    $(".resmenu").toggleClass("mobilemenu");
+    $(this).toggleClass("active");
+    $("header #hamburger a .fa").toggle();
+  });
+
+  $("a.1").click(function() {
+    $("#modal1").addClass('activate');
+    $("body").addClass("modal-open");
+  });
+  $("a.2").click(function() {
+    $("#modal2").addClass('activate');
+    $("body").addClass("modal-open");
+  });
+  $(".closemodal").click(function() {
+    $("#modal1").removeClass('activate');
+    $("#modal2").removeClass('activate');
+    $("body").removeClass("modal-open");
+  });
+
+  $(window).resize(function() {
+      hideMobileMenu(false);
+  });
+
+  $(function(){
+  	// Instantiate MixItUp:
+  	$('#Container').mixItUp();
+  });
+
+});
